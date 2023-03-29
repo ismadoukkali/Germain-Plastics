101456f535780dfe91c50164f1b7597ae2d010ba
ismadoukkali <ismaelitope@gmail.com>
2023-03-29 19:59:41 +0200

    modified:   app.py

--- app.py
+++ app.py
@@ -49,7 +49,10 @@ def find_most_similar_name(text, names):
             most_similar_name = name
     if text == 'Woven geotextile':
         most_similar_name = 'Product coming soon.....png'
-        
+
+    if "lite" not in text or "Lite" not in text and "HDPE Liner" in text:
+        most_similar_name = 'HDPE liners (Geomembranes).png' 
+    
     if highest_score < 0.4:
         most_similar_name = 'Product coming soon.....png'
     return most_similar_name
